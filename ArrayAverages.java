3e7bf84aa4169a8286958487a9c448b30eb93fa4
Walkerwalt <walker.burden@stu.daviess.kyschools.us>
2026-02-11 21:03:35 +0000

    Commit 1

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -9,17 +9,66 @@ public class ArrayAverages{
   }
 
   public void rowAverages(){
-    
+    //Double since division happens (running total)
+    double avg = 0;
+
+    //Traditional for in row major to accumulate the sum
+    for(int row = 0; row < ray.length; row ++)
+      {
+        for(int col = 0; col < ray[row].length; col ++)
+        {
+          avg += ray[row][col];
+        }
+        
+    //Divide by the length of columns
+      avg /= ray[0].length;
+      
+    //Print the average
+      System.out.println("Row " + row + " average: " + avg);
+
+    //Reset the variable
+      avg = 0;
+      }
+
   }
 
   public void columnAverages(){
     //Column major order
     //   Take each column and go through all of the rows
     //   Top down and Left to Right
+
+    //Double since division happens (running total)
+    double avg = 0;
+
+    //Traditional for in row major to accumulate the sum
+    for(int col = 0; col < ray[0].length; col ++)
+      {
+        for(int row = 0; row < ray.length; row ++)
+        {
+          avg += ray[row][col];
+        }
+        
+    //Divide by the length
+      avg /= ray.length;
+      
+      System.out.println("Column " + col + " average: " + avg);
+
+      avg = 0;
+      }
   }
 
   public void arrayAverage(){
+    double avg = 0;
+    for(int row = 0; row < ray.length; row ++)
+      {
+        for(int col = 0; col < ray[row].length; col ++)
+        {
+          avg += ray[row][col];
+        }
+      }
+      avg /= ray.length * ray[0].length;
 
+      System.out.println("Array average: " + avg);
   }
 
   public String toString(){
--- Gee21.java
+++ Gee21.java
@@ -10,6 +10,20 @@ public class Gee21{
       15 16 17 18 19 20 21
       22 23 24 25...
       */
+  
+      //Used as a holder to know what value to add
+      int popularGuy = 1;
+
+      //Traditional for in row major order 
+      for(int row = 0; row < ray.length; row ++)
+      {
+        for(int col = 0; col < ray[row].length; col ++)
+        {
+          //popularGuy started at 1 so add him the the array first then increase his value
+          ray[row][col] = popularGuy;
+          popularGuy++;
+        }
+      }
 
     ArrayAverages twoDee = new ArrayAverages(ray);
 
